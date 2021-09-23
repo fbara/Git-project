15101b6ad7138a99a86eb1eef938afc7b46d5cf7
fbara <fbara1@yahoo.com>
2021-09-23 09:07:44 -0500

    Content added.

---
+++ Git project/ModalView.swift
@@ -0,0 +1,20 @@
+//
+//  ModalView.swift
+//  Git project
+//
+//  Created by Frank Bara on 9/23/21.
+//
+
+import SwiftUI
+
+struct ModalView: View {
+    var body: some View {
+        Text("This is the modal view")
+    }
+}
+
+struct ModalView_Previews: PreviewProvider {
+    static var previews: some View {
+        ModalView()
+    }
+}
