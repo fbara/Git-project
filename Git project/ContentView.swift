49dcdb24897c964043408408953f316dfa61fff3
fbara <fbara1@yahoo.com>
2021-09-23 08:52:18 -0500

    Initial Commit

---
+++ Git project/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Git project
+//
+//  Created by Frank Bara on 9/23/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Git project/Git_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Git_projectApp.swift
+//  Git project
+//
+//  Created by Frank Bara on 9/23/21.
+//
+
+import SwiftUI
+
+@main
+struct Git_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
